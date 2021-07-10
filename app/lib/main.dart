4ea2af15669c0c78e19df6c82b487832cb8f67ca
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-07-10 05:34:28 +0530

    add push notification

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  "high_importance_channel",
+  "High Importance Notifications",
+  "description go brrr",
+  importance: Importance.high,
+  playSound: true,
+);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingbackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("bg message hehe ${message.messageId}");
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingbackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
   runApp(MyApp());
 }
 
@@ -25,9 +55,77 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(
+                    channel.id, channel.name, channel.description,
+                    color: Colors.amber,
+                    playSound: true,
+                    icon: '@mipmap/ic_launcher')));
+      }
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print("new notif");
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
+        showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: Text(notification.title),
+                content: Column(
+                  children: <Widget>[Text(notification.body)],
+                ),
+              );
+            });
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-    );
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Icon(Icons.medical_services_outlined),
+                Text("Docthru",
+                    style:
+                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold))
+              ],
+            ),
+            Center(
+              child: MaterialButton(
+                onPressed: () {
+                  flutterLocalNotificationsPlugin.show(
+                      0,
+                      "testing",
+                      "lol shoulda work",
+                      NotificationDetails(
+                          android: AndroidNotificationDetails(
+                              channel.id, channel.name, channel.description,
+                              color: Colors.red,
+                              importance: Importance.high,
+                              icon: '@mipmap/ic_launcher',
+                              playSound: true)));
+                },
+                child: Text("Button"),
+              ),
+            )
+          ],
+        ));
   }
 }
