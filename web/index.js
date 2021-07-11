f842d43297a43654fb899c6c3d48b6c02fd1c24d
shruti <shrutigupta5555@gmail.com>
2021-07-11 18:35:56 +0530

    update

--- web/index.js
+++ web/index.js
@@ -12,124 +12,128 @@ let continuousPicture;
 //threshold values
 
 
+function sendEmail() {
+    
+}
+
+sendEmail();
+const heartrate = [90,100]
+const firstbp = [90, 130]
+const secondbp = [70,100]
+const temp = [97,99]
+const respiration = [16,20]
+const oxygen = [92,100]
 
 
-// const heartrate = [90,100]
-// const firstbp = [90, 130]
-// const secondbp = [70,100]
-// const temp = [97,99]
-// const respiration = [16,20]
-// const oxygen = [92,100]
+const ref = [heartrate, firstbp, secondbp, temp, respiration, oxygen]
 
 
-// const ref = [heartrate, firstbp, secondbp, temp, respiration, oxygen]
+function compare(temp) {
 
-
-// function compare(temp) {
-
-//     for(let i=0; i < temp.length; i++){
-//         console.log(ref[i])
-//         if(temp[i] < ref[i][0] || temp[i] >= ref[i][1]){
+    for(let i=0; i < temp.length; i++){
+        console.log(ref[i])
+        if(temp[i] < ref[i][0] || temp[i] >= ref[i][1]){
             
 
-//             //send Email
-//         }
-//     }
+            //send Email
+            sendEmail();
+        }
+    }
 
-// }
+}
 
 
-// webcam.start()
-//     .then(result =>{
-//         console.log("webcam started");
+webcam.start()
+    .then(result =>{
+        console.log("webcam started");
   
-//     })
-//     .catch(err => {
-//         console.log(err);
-// });
+    })
+    .catch(err => {
+        console.log(err);
+});
 
-// stopBtn.addEventListener("click", () => {
-//     webcam.stop()
-//     clearInterval(continuousPicture);
-// });
-// flipBtn.addEventListener("click", () => {
-//     webcam.flip();
-// });
+stopBtn.addEventListener("click", () => {
+    webcam.stop()
+    clearInterval(continuousPicture);
+});
+flipBtn.addEventListener("click", () => {
+    webcam.flip();
+});
 
-// btn.addEventListener("click", () => {
-//     // recognize();
-//     continuousPicture = setInterval(recognize, 10000);
-// });
-// function recognize() {
-//     // console.log("hi")
-//     let picture = webcam.snap();
-//     let temp = []
-//     let item;
-//     let n;
-//     let nitem;
+btn.addEventListener("click", () => {
+    // recognize();
+    continuousPicture = setInterval(recognize, 10000);
+});
+function recognize() {
+    // console.log("hi")
+    let picture = webcam.snap();
+    let temp = []
+    let item;
+    let n;
+    let nitem;
     
    
 
 
-//        Tesseract.recognize(
-//         picture,
-//         'eng',
+       Tesseract.recognize(
+        picture,
+        'eng',
         
-//       ).then(({ data: { text } }) => {
+      ).then(({ data: { text } }) => {
         
-//         const output = text.split("\n");
+        const output = text.split("\n");
 
 
         
-//         vd.textContent = text;
+        vd.textContent = text;
 
         
         
-//            //parse data
-//             for(let i=0; i < output.length; i++){
+           //parse data
+            for(let i=0; i < output.length; i++){
 
-//                 // console.log(output[i])
-//                 if(output[i].includes("/")) {
-//                     n = output[i].split("/");
-//                     for(let j=0; j < n.length; j++){
-//                         nitem = parseInt(n[j])
-//                         if(!isNaN(nitem)){
-//                             temp.push(nitem)
-//                         }
-//                     }
-//                 }else{
-//                     item = parseInt(output[i])
-//                     if(!isNaN(item)){
-//                         temp.push(item)
-//                     }
-//                 }
+                // console.log(output[i])
+                if(output[i].includes("/")) {
+                    n = output[i].split("/");
+                    for(let j=0; j < n.length; j++){
+                        nitem = parseInt(n[j])
+                        if(!isNaN(nitem)){
+                            temp.push(nitem)
+                        }
+                    }
+                }else{
+                    item = parseInt(output[i])
+                    if(!isNaN(item)){
+                        temp.push(item)
+                    }
+                }
                 
-//             }
+            }
             
         
-//         console.log(temp);
-//         //compare values
-//         compare(temp)
+        console.log(temp);
+        //compare values
+        compare(temp)
 
 
      
-//     })
+    })
 
  
     
     
     
    
-//     // console.log("dwnld")
-//     // document.querySelector('#download-photo').href = picture;
+    // console.log("dwnld")
+    // document.querySelector('#download-photo').href = picture;
 
-//     // Tesseract.recognize(
-//     //     picture,
-//     //     'eng',
-//     //     { logger: m => console.log(m) }
-//     //   ).then(({ data: { text } }) => {
-//     //     console.log(text);
-//     //     vd.textContent = text;
-//     // })
-// }
+    // Tesseract.recognize(
+    //     picture,
+    //     'eng',
+    //     { logger: m => console.log(m) }
+    //   ).then(({ data: { text } }) => {
+    //     console.log(text);
+    //     vd.textContent = text;
+    // })
+}
 
