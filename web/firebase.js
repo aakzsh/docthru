135aee081e443cc8185916624cb4451508830317
shruti <shrutigupta5555@gmail.com>
2021-07-11 19:53:34 +0530

    dont know

---
+++ web/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC69JnRQZxYSQnwZxvZpHULGht92RGgM_w",
+    authDomain: "docthru.firebaseapp.com",
+    projectId: "docthru",
+    storageBucket: "docthru.appspot.com",
+    messagingSenderId: "1050541674869",
+    appId: "1:1050541674869:web:0a10c38685294272bbc33c",
+    measurementId: "G-F9G9B49DCT"
+  };
+
+  
